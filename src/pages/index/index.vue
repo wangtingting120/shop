8ff417ddab6950c47921411e64ffcefbcac1e5da
wangtingting120 <1500639792@qq.com>
2019-12-02 15:49:11 +0800

    second

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -59,9 +59,9 @@ export default {
 
       ],
       imgUrls:[
-        'https://image.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-        'https://image.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-        'https://image.unsplash.com/photo-1551446591-142875a901a1?w=640'
+        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+        'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
 
       ],
       indicatorDots: false,
--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -1,52 +1,40 @@
 <template>
-  <div style="width: 100%;height:100%">
-    <map 
-    id="map"
-    scale="14"
-    ：markers="markers"
-    show-location
-    style="width: 100%;height: 100%"
+  <div style="width: 100%; height: 100%;">
+    <map
+      id="map"
+      scale="14"
+      :markers="markers"
+      show-location
+      style="width: 100%; height: 100%;"
     ></map>
-
-
   </div>
- 
 </template>
 
 <script>
-
-
 export default {
   data () {
-    return {
-      markers:[
-        {
-      
-          id:0,
-          latitude: 23.099994,
-          longitude: 113.324520,
-          width: 50,
-          height :50
-        }]
-      
-      }
-   
+    return {      
+      markers: [{
+        id: 0,
+        latitude: 23.099994,
+        longitude: 113.324520,
+        width: 50,
+        height: 50
+      }]
+    }
   },
 
   methods: {
-   
-    },
+  },
 
   created () {
- 
   }
 }
 </script>
 
-<style >
-body,html{
+<style>
+body, html {
   height:100%
 }
-
-
 </style>
+
