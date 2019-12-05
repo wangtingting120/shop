651de9b9c8642b7edef050f2136f7686ba75dcc3
wangtingting120 <1500639792@qq.com>
2019-12-05 20:59:16 +0800

    4.1

--- src/pages/shop/index.vue
+++ src/pages/shop/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-
-
+    <h1>标签 </h1>
+  <div> ceshi1</div>
+<i-panel title="基础用法">
+    <i-input value=" " title="收货人" autofocus placeholder="名字" />
+    <i-input value=" " type="number" title="联系电话" placeholder="请输入手机号" />
+    <i-input value=" " type="textarea" title="详细地址" placeholder="请输入详细地址(最多50字)" maxlength="50" />
+    <i-input value=" " title="用户信息" />
+</i-panel>
 </div>
  
 </template>
@@ -11,15 +17,19 @@
 
 export default {
   data () {
-    return {
-    
 
+    return {
+     
+     
       
       }
    
   },
 
   methods: {
+    
+
+
     
    
     },
@@ -31,10 +41,6 @@ export default {
 </script>
 
 <style scoped>
-div >>> .no-border{
-  border-width:0pt;
-}
-div >>> .split{
-  margin-bottom:10pt;
-}
+
 </style>
+
--- src/pages/show/index.vue
+++ src/pages/show/index.vue
@@ -3,7 +3,10 @@
   
 <i-card title="哈哈" extra="..." thumb="/static/images/001.png">
     <view slot="content"><image src="/static/images/20.jpg"></image></view>
-    <view slot="footer">这件衣服很不错哦 点赞</view>
+   <view slot="footer">
+   123
+   </view>
+  
 </i-card>
 <i-card title="哈皮" extra="..." thumb="/static/images/002.jpg">
     <view slot="content"><image src="/static/images/20.jpg"></image></view>
@@ -11,25 +14,33 @@
 </i-card>
 <i-card title="11" extra="..." thumb="/static/images/003.png">
     <view slot="content">   
-       <swiper
-    :indicator-dots="indicatorDots"
-    :autoplay="autoplay"
-    :interval="interval"
-    :duration="duration"
-    >
-    <block>
+       <swiper autoplay circle indicator-dots>
+ 
       <swiper-item>
-        <image src="/static/images/20.jpg"></image>
-        <image src="/static/images/21.jpg"></image>
+        <image src="/static/images/20.jpg" ></image>
       </swiper-item>
-      </block>
-      </swiper></view>
+  
+
+      <swiper-item>
+        <image src="/static/images/21.jpg" ></image>
+      </swiper-item>
+   
+      </swiper>
+       
     <view slot="footer">喜欢 点赞</view>
 </i-card>
 <i-card title="23" extra="..." thumb="/static/images/004.png">
     <view slot="content"><image src="/static/images/20.jpg"></image></view>
     <view slot="footer">爱了爱了</view>
 </i-card>
+
+<i-panel title="基础用法">
+    <i-input value="{{ value1 }}" title="收货人" autofocus placeholder="名字" />
+    <i-input value="{{ value2 }}" type="number" title="联系电话" placeholder="请输入手机号" />
+    <i-input value="{{ value3 }}" type="textarea" title="详细地址" placeholder="请输入详细地址(最多50字)" maxlength="50" />
+    <i-input value="{{ value4 }}" title="用户信息" disabled />
+</i-panel>
+
 </div>
  
 </template>
